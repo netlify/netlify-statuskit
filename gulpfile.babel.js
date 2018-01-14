fe002415ee8a8c6ec3e4a6d51339e51aa522b6d1
Matthew Somerville <matthew-github@dracos.co.uk>
2018-01-14 22:14:25 +0000

    Remove webpack and other unused things.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,9 @@
 import gulp from "gulp";
 import cp from "child_process";
-import gutil from "gulp-util";
+import babel from "gulp-babel";
 import postcss from "gulp-postcss";
-import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
-import webpack from "webpack";
-import webpackConfig from "./webpack.conf";
 import inquirer from "inquirer";
 import toml from "tomljs";
 import fs from "fs";
@@ -28,23 +25,20 @@ gulp.task("build-preview", ["css", "js", "hugo-preview"]);
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
-    .pipe(postcss([cssnext(), cssImport({from: "./src/css/main.css"})]))
+    .pipe(postcss([cssnext()]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
 ));
 
-gulp.task("js", (cb) => {
-  const myConfig = Object.assign({}, webpackConfig);
+gulp.task("js", () => (
+  gulp.src("./src/js/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("./dist/js"))
+));
 
-  webpack(myConfig, (err, stats) => {
-    if (err) throw new gutil.PluginError("webpack", err);
-    gutil.log("[webpack]", stats.toString({
-      colors: true,
-      progress: true
-    }));
-    browserSync.reload();
-    cb();
-  });
+gulp.task("js-watch", ["js"], function(done) {
+  browserSync.reload();
+  done();
 });
 
 gulp.task("server", ["hugo", "css", "js"], () => {
@@ -53,7 +47,7 @@ gulp.task("server", ["hugo", "css", "js"], () => {
       baseDir: "./dist"
     }
   });
-  gulp.watch("./src/js/**/*.js", ["js"]);
+  gulp.watch("./src/js/**/*.js", ["js-watch"]);
   gulp.watch("./src/css/**/*.css", ["css"]);
   gulp.watch("./site/**/*", ["hugo"]);
 });
